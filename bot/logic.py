a11460b44463a76b9feded84146782775c36026a
Andrea Aprile <andy@LenovoAndrea.localdomain>
2026-02-27 16:14:36 +0100

    controllo password sicura

--- bot/logic.py
+++ bot/logic.py
@@ -10,3 +10,21 @@ def generate_password(num_words: int=4) -> str:
 			word=secrets.choice(word_list)
 			words.append(word)
 		return "-".join(words)
+		
+def is_strong_password(password: str) -> bool:
+	is_long_enough=len(password)>=15
+	has_digit=False
+	has_upper=False
+	has_symbol=False
+	for c in password:
+		if c.isdigit():
+			has_digit=True
+		if c.isupper():
+			has_upper=True
+		if c in string.punctuation:
+			has_symbol=True
+	return is_long_enough and has_digit and has_upper and has_symbol
+
+	
+			
+	
