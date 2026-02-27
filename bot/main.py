e2ea043c9aade0e9c1aea7d474bae3c4e5a25946
calabresegabriel78-dotcom <calabresegabriel78@gmail.com>
2026-02-27 16:07:53 +0100

    implementa comando start e configurazione base del bot

---
+++ bot/main.py
@@ -0,0 +1,16 @@
+from telegram import Update 
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+from bot.logic import generate_password, is_strong_password, generate_pin, cesar_cipher
+
+TOKEN="8658709912:AAG-OHzXA-tQfPXWHWXuOS-v1cMb-2fglrA"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Bot Generatore di Password\n\n"
+        "Comandi:\n"
+        "/password <numero_parole>\n"
+        "/check <tua_password> - Verifica se la password è sicura\n"
+        "/pin <numero_cifre> - Genera un pin\n"
+        "/encrypt <password, shift> - Applica il cifrario di Cesare alla password"
+    )
