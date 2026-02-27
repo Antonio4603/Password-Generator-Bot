77a66c3a5dbff3aa90e3aa54433c694b4621091b
gabrielcalabreseunict <calabresegabriel78@gmail.com>
2026-02-27 17:39:20 +0100

    Fix generale

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,4 @@
-from telegram import Update 
+from telegram import UpdateÂ 
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 from bot.logic import generate_password, is_strong_password, generate_pin, cesar_cipher
@@ -6,29 +6,28 @@ from bot.logic import generate_password, is_strong_password, generate_pin, cesar
 TOKEN="8658709912:AAG-OHzXA-tQfPXWHWXuOS-v1cMb-2fglrA"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Bot Generatore di Password\n\n"
-        "Comandi:\n"
-        "/password <numero_parole>\n"
-        "/check <tua_password> - Verifica se la password è sicura\n"
-        "/pin <numero_cifre> - Genera un pin\n"
-        "/encrypt <password, shift> - Applica il cifrario di Cesare alla password"
-    )
-    
+Â  Â  await update.message.reply_text(
+Â  Â  Â  Â  "Bot Generatore di Password\n\n"
+Â  Â  Â  Â  "Comandi:\n"
+Â  Â  Â  Â  "/password <numero_parole>\n"
+Â  Â  Â  Â  "/check <tua_password> - Verifica se la password Ã¨ sicura\n"
+Â  Â  Â  Â  "/pin <numero_cifre> - Genera un pin\n"
+Â  Â  Â  Â  "/encrypt <password, shift> - Applica il cifrario di Cesare alla password"
+Â  Â  )
+Â  Â Â 
 async def password(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not context.args:
-        await update.message.reply_text("Inserisci la lunghezza.")
-        return
-    try:
-        num_words=int(context.args[0])
-        pwd=generate_password(num_words)
-        await update.message.reply_text(f"Password generata:\n{pwd}")
-    except ValueError:
-        await update.message.reply_text("Lunghezza non valida (min 4).")
+Â  Â  if not context.args:
+Â  Â  Â  Â  await update.message.reply_text("Inserisci la lunghezza.")
+Â  Â  Â  Â  return
+Â  Â  try:
+Â  Â  Â  Â  num_words=int(context.args[0])
+Â  Â  Â  Â  pwd=generate_password(num_words)
+Â  Â  Â  Â  await update.message.reply_text(f"Password generata:\n{pwd}")
+Â  Â  except ValueError:
+Â  Â  Â  Â  await update.message.reply_text("Lunghezza non valida (min 4).")
 
 if __name__ == "__main__":
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("password", password))
-    app.run_polling()
-che commit devo scrivere?
+Â  Â  app = ApplicationBuilder().token(TOKEN).build()
+Â  Â  app.add_handler(CommandHandler("start", start))
+Â  Â  app.add_handler(CommandHandler("password", password))
+Â  Â  app.run_polling()
